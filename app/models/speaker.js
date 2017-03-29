7a16d454168850efa51eb967b5b39a986c57e005
Edward Faulkner <ef@alum.mit.edu>
2017-03-29 01:46:33 -0700

    dropping unused fields

--- app/models/speaker.js
+++ app/models/speaker.js
@@ -1,16 +1,11 @@
 import DS from 'ember-data';
-const { attr, hasMany } = DS;
+const { attr } = DS;
 import Ember from 'ember';
 
 export default DS.Model.extend({
   name: attr('string'),
-  twitter: attr('string'),
   photoURL: attr('string'),
-  bioHTML: attr('string'),
-  bioText: attr('string'),
   bio: attr({ fieldType: '@cardstack/mobiledoc' }),
-  tags: hasMany(),
-  sessions: hasMany(),
   slug: attr('string'),
   type: Ember.computed(function () {
     return this.constructor.modelName;

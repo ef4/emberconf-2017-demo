936d006716cef44f5b499c453392c5c71a1ef34b
Edward Faulkner <ef@alum.mit.edu>
2017-03-28 18:02:06 -0700

    adding some routes

--- app/adapters/speaker.js
+++ app/adapters/speaker.js
@@ -1,4 +1,26 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
+  shouldReloadRecord() {
+    return false;
+  },
+  shouldBackgroundReloadRecord() {
+    return false;
+  },
+  findRecord(store, type, id /*, snapshot */) {
+    return this.ajax('/speakers').then(document => {
+      let matching;
+      let other = document.data.filter(d => {
+        if (String(d.id) === id) {
+          matching = d;
+        } else {
+          return true;
+        }
+      });
+      return {
+        data: matching,
+        included: document.included.concat(other)
+      };
+    });
+  }
 });
--- app/router.js
+++ app/router.js
@@ -7,6 +7,8 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('speakers', { path: '/' });
+  this.route('speaker', { path: '/speakers/:id' });
 });
 
 export default Router;
